25a7df2d1c978991171a08fe3056a89a937a0e67
Vadim Markovtsev <v.markovtsev@samsung.com>
2015-05-29 17:50:26 +0300

    Fixed viewerjs; added footer info

--- src/js/download.js
+++ src/js/download.js
@@ -3,6 +3,7 @@ if (navigator.userAgent.search("Ubuntu") >= 0) {
     download.hide();
     var apt_get = $("<pre class=\"ubuntu-download\">" +
         "wget -O - https://velesnet.ml/apt/velesnet.ml.gpg.key | sudo apt-key add -\n" +
+        "echo \"deb https://velesnet.ml/apt trusty main\" | sudo tee -a /etc/apt/sources.list\n" +
         "sudo apt-get update && sudo apt-get install python3-veles</pre>");
     download.after(apt_get);
 }
